27d91cd0541d6b76c7c72428450dbe5696aece9e
BenMcFadyen <ben.mcfadyen3@gmail.com>
2019-03-08 15:47:18 +0000

    add file selector, predict button logic

--- src/app/selection/selection.component.ts
+++ src/app/selection/selection.component.ts
@@ -9,7 +9,9 @@ import { ModelData } from '../ModelData';
 })
 export class SelectionComponent implements OnInit 
 {
+	imgURL: string
 	epsilon: number = 1;
+	selectedModel: string
 
   	constructor(private modelService: ModelService) 
   	{ 
@@ -22,7 +24,33 @@ export class SelectionComponent implements OnInit
 	}
 
 
-	async onEpsilonChange(value)
+
+	onSelectFile(event) 
+	{ 	
+	    const file = event.target.files[0]
+		
+		if (event.target.files && event.target.files[0]) 
+		{
+   			const file = event.target.files[0]
+
+			var reader = new FileReader()
+			reader.readAsDataURL(event.target.files[0]); // read file as data url
+
+			// called once readAsDataURL is completed
+			reader.onload = (event:any) => {this.imgURL = event.target.result;}
+		}
+	}
+
+	onPredictButtonClick(modelSelection)
+	{
+		var selectedModelName = this.selectedModel
+
+		console.log("Predicting: "  + selectedModelName)
+
+	}
+
+
+	onEpsilonChange(value)
 	{
 		// TODO: Should max epsilon value be 100?
 		if(value > 100)
