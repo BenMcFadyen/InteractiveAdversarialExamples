cbf131c958ba45b9639c0db9d6204604363152ce
BenMcFadyen <ben.mcfadyen3@gmail.com>
2019-03-08 12:28:45 +0000

    Renamed ModelInfo to ModelData, added additonal params

---
+++ src/app/ModelData.ts
@@ -0,0 +1,20 @@
+import * as tf from '@tensorflow/tfjs';
+
+export class ModelData
+{
+	name: string;
+	model: tf.Model;
+	loaded: boolean;
+	imgHeight:number;
+	imgWidth:number;
+	imgChannels:number;
+
+	constructor(name, model, imgHeight, imgWidth, imgChannels)
+	{
+		this.name = name;
+		this.model = model;
+		this.imgHeight = imgHeight;
+		this.imgWidth = imgWidth;	
+		this.imgChannels = imgChannels;
+	}	
+}
