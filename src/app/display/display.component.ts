ae67d03b7abe64d445cba33982db3ec04c986673
BenMcFadyen <ben.mcfadyen3@gmail.com>
2019-03-09 13:40:08 +0000

    updated css tags/prediction displaying

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Prediction } from '../Prediction';
 
 @Component({
   selector: 'app-display',
   templateUrl: './display.component.html',
   styleUrls: ['./display.component.scss']
 })
-export class DisplayComponent implements OnInit {
+export class DisplayComponent implements OnInit
+{
+
+ 	originalPredictions: Prediction[] = [new Prediction('test', 'test2')]
+ 	differencePredictions: Prediction[]
+ 	adversarialPredictions: Prediction[]
 
 	constructor() 
 	{
