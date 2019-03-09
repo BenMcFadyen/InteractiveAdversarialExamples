6b0361a0f6eb57badb930843b77c178bfd7a8064
BenMcFadyen <ben.mcfadyen3@gmail.com>
2019-03-09 10:21:41 +0000

    cleanup, addition of getResizedCanvasFromExisting

--- src/app/image.service.ts
+++ src/app/image.service.ts
@@ -17,16 +17,34 @@ export class ImageService
 	*/
 	drawIMGToCanvas(img:HTMLImageElement, canvasID:string, desiredCanvasHeight:number = 299, desiredCanvasWidth:number = 299)
 	{
-		var canvas = <HTMLCanvasElement> document.getElementById(canvasID);
-		canvas.height = desiredCanvasHeight;
-		canvas.width = desiredCanvasWidth;		
-		var context = canvas.getContext("2d");
-		context.drawImage(img,0,0,desiredCanvasHeight,desiredCanvasWidth);
+		var canvas = <HTMLCanvasElement> document.getElementById(canvasID)
+		canvas.height = desiredCanvasHeight
+		canvas.width = desiredCanvasWidth
+		var context = canvas.getContext("2d")
+		context.drawImage(img,0,0,desiredCanvasHeight,desiredCanvasWidth)
 	}
 
 
+	/**
+ 	* @returns a resized HTMLCanvasElement of the original canvasID
+	*/
+	getResizedCanvasFromExisting(canvasID:string, desiredCanvasHeight:number = 299, desiredCanvasWidth:number = 299)
+	{
+		var canvas = <HTMLCanvasElement> document.getElementById(canvasID)
+		var resizedCanvas = <HTMLCanvasElement> document.createElement('canvas')
+		resizedCanvas.id = 'resizedCanvas'
+		resizedCanvas.height = desiredCanvasHeight
+		resizedCanvas.width = desiredCanvasWidth
+
+		var context = resizedCanvas.getContext("2d")
+		context.drawImage(canvas, 0, 0, desiredCanvasHeight, desiredCanvasWidth)		
+		return resizedCanvas
+	}
 
 
+	/*
+		resizes an existing canvas, from itself
+	*/
 	resizeExistingCanvas(canvasID:string, desiredCanvasHeight:number = 299, desiredCanvasWidth:number = 299)
 	{	
 		/*
@@ -34,23 +52,17 @@ export class ImageService
 		* re-draw the existing canvas to this element (at the desired resize)
 		* re-draw this to the original canvas	
 		*/
+		
+		var canvas = <HTMLCanvasElement> document.getElementById(canvasID)
+		var tempCanvas = this.getResizedCanvasFromExisting(canvasID, desiredCanvasHeight, desiredCanvasWidth)
 
-		var canvas = <HTMLCanvasElement> document.getElementById(canvasID);
-		var tempCanvas = <HTMLCanvasElement> document.createElement('canvas');
-		tempCanvas.id = 'tempCanvas';
-		tempCanvas.height = desiredCanvasHeight;
-		tempCanvas.width = desiredCanvasWidth;		
-
-		var tempContext = tempCanvas.getContext("2d");
-		tempContext.drawImage(canvas, 0, 0, desiredCanvasHeight, desiredCanvasWidth);
-
-		canvas.height = desiredCanvasHeight;
-		canvas.width = desiredCanvasWidth;		
-		var context = canvas.getContext("2d");	
-		context.drawImage(tempCanvas, 0, 0, desiredCanvasHeight, desiredCanvasWidth);
+		canvas.height = desiredCanvasHeight
+		canvas.width = desiredCanvasWidth
+		var context = canvas.getContext("2d")	
+		context.drawImage(tempCanvas, 0, 0, desiredCanvasHeight, desiredCanvasWidth)
 
 		// cleanup the temporary canvas
-		tempCanvas.remove();	
+		tempCanvas.remove()	
 	}	
 
 
@@ -64,12 +76,12 @@ export class ImageService
 		var canvas = <HTMLCanvasElement> document.getElementById(canvasID)
 
 		// Convert the canvas pixels to a Tensor of the matching shape
-		let tensor = tf.fromPixels(canvas, reqNumberChannels);
+		let tensor = tf.fromPixels(canvas, reqNumberChannels)
 
-		var reshapedTensor = tf.reshape(tensor, [1, reqTensorHeight, reqTensorWidth, reqNumberChannels]);
-		reshapedTensor = tf.cast(reshapedTensor, 'float32');
+		var reshapedTensor = tf.reshape(tensor, [1, reqTensorHeight, reqTensorWidth, reqNumberChannels])
+		reshapedTensor = tf.cast(reshapedTensor, 'float32')
 
 		console.log(reshapedTensor)
-		return reshapedTensor;
+		return reshapedTensor
 	}
 }
