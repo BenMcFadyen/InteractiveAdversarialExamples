5a16baceb40fb5c6c4853f894db8d81121f1fc32
BenMcFadyen <ben.mcfadyen3@gmail.com>
2019-03-09 10:10:50 +0000

    added resize canvas function

--- src/app/image.service.ts
+++ src/app/image.service.ts
@@ -22,22 +22,54 @@ export class ImageService
 		canvas.width = desiredCanvasWidth;		
 		var context = canvas.getContext("2d");
 		context.drawImage(img,0,0,desiredCanvasHeight,desiredCanvasWidth);
+	}
+
+
+
+
+	resizeExistingCanvas(canvasID:string, desiredCanvasHeight:number = 299, desiredCanvasWidth:number = 299)
+	{	
+		/*
+		* create a temporary canvas element
+		* re-draw the existing canvas to this element (at the desired resize)
+		* re-draw this to the original canvas	
+		*/
+
+		var canvas = <HTMLCanvasElement> document.getElementById(canvasID);
+		var tempCanvas = <HTMLCanvasElement> document.createElement('canvas');
+		tempCanvas.id = 'tempCanvas';
+		tempCanvas.height = desiredCanvasHeight;
+		tempCanvas.width = desiredCanvasWidth;		
+
+		var tempContext = tempCanvas.getContext("2d");
+		tempContext.drawImage(canvas, 0, 0, desiredCanvasHeight, desiredCanvasWidth);
+
+		canvas.height = desiredCanvasHeight;
+		canvas.width = desiredCanvasWidth;		
+		var context = canvas.getContext("2d");	
+		context.drawImage(tempCanvas, 0, 0, desiredCanvasHeight, desiredCanvasWidth);
+
+		// cleanup the temporary canvas
+		tempCanvas.remove();	
 	}	
+
+
 
 	/**
 	* Grabs image data from given canvas, returns tf.tensor
 	* @returns reshapedTensor
 	*/
-    getTensorFromCanvas(canvasID: string, reqTensorHeight: number, reqTensorWidth, reqNumberChannels: number)
+    getTensorFromCanvas(canvasID: string, reqTensorHeight: number, reqTensorWidth: number, reqNumberChannels: number)
 	{
 		var canvas = <HTMLCanvasElement> document.getElementById(canvasID)
 
 		// Convert the canvas pixels to a Tensor of the matching shape
 		let tensor = tf.fromPixels(canvas, reqNumberChannels);
+
 		var reshapedTensor = tf.reshape(tensor, [1, reqTensorHeight, reqTensorWidth, reqNumberChannels]);
 		reshapedTensor = tf.cast(reshapedTensor, 'float32');
-		console.log(reshapedTensor);	
 
+		console.log(reshapedTensor)
 		return reshapedTensor;
 	}
 }
