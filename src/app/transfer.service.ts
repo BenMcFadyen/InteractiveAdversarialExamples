f1182368de3982e44a62400cb6b0ecff35d399be
BenMcFadyen <ben.mcfadyen3@gmail.com>
2019-03-09 18:31:06 +0000

    split predictions into their 3 distinct categories

--- src/app/transfer.service.ts
+++ src/app/transfer.service.ts
@@ -7,37 +7,62 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class TransferService 
 {
- 	private predictions: Prediction[][] = 
- 	[
- 		[new Prediction('A', 'B'),
- 		 new Prediction('C', 'D'),
- 		 new Prediction('E', 'F')],
 
- 		[new Prediction('1', '2'),
- 		 new Prediction('3', '4'),
- 		 new Prediction('5', '6')],
 
- 		[new Prediction('7', '8'),
- 		 new Prediction('9', '10'),
- 		 new Prediction('11', '12')],
-	]
 
-	private predictionsSource = new BehaviorSubject(this.predictions);
-	currentPredictions = this.predictionsSource.asObservable();
+ 	private originalPredictions: Prediction[] = [new Prediction('A', 'B'),
+										 		 new Prediction('C', 'D'),
+										 		 new Prediction('E', 'F')]
+
+ 	private differencePredictions: Prediction[] = [new Prediction('1', '2'),
+										 		   new Prediction('3', '4'),
+										 		   new Prediction('5', '6')] 	
+
+ 	private adversarialPredictions: Prediction[] = [new Prediction('7', '8'),
+											 		new Prediction('9', '10'),
+											 		new Prediction('11', '12')]
+
+
+	private originalPredictionsSource = new BehaviorSubject(this.originalPredictions);
+	private differencePredictionsSource = new BehaviorSubject(this.differencePredictions);
+	private adversarialPredictionsSource = new BehaviorSubject(this.adversarialPredictions);
+
+	currentoriginalPredictions = this.originalPredictionsSource.asObservable();
+	currentdifferencePredictions= this.differencePredictionsSource.asObservable();
+	currentadversarialPredictionsSource = this.adversarialPredictionsSource.asObservable();
 
 	constructor() 
 	{ 
 
 	}
 
-
-	setPredictions(predictions: Prediction[][])
+	setOriginalPredictions(originalPredictions: Prediction[])
 	{
-		this.predictionsSource.next(predictions)
+		this.originalPredictionsSource.next(originalPredictions)
 	}
 
-	getPredictions() : Prediction[][]
+	getOriginalPredictions() : Prediction[]
 	{
-		return this.predictions
+		return this.originalPredictions
+	}
+
+	setDifferencePredictions(differencePredictions: Prediction[])
+	{
+		this.differencePredictionsSource.next(differencePredictions)
+	}
+
+	getDifferencePredictions() : Prediction[]
+	{
+		return this.differencePredictions
+	}
+
+	setAdversarialPredictions(adversarialPredictions: Prediction[])
+	{
+		this.adversarialPredictionsSource.next(adversarialPredictions)
+	}
+
+	getAdversarialPredictions() : Prediction[]
+	{
+		return this.adversarialPredictions
 	}
 }
