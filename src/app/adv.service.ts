45e4984bd8698182be0af833da983f2533644266
BenMcFadyen <ben.mcfadyen3@gmail.com>
2019-03-10 18:30:28 +0000

    added Targeted-FGSM added imagenet target class selector

--- src/app/adv.service.ts
+++ src/app/adv.service.ts
@@ -75,6 +75,55 @@ export class AdvService
 		return perturbations
 	}
 
+
+
+	async genAdvPerturbationFGSMTargeted(modelObject: ModelData, targetPrediction:string, img3, img4, epsilon = 0.025)
+	{
+		var model = modelObject.model
+
+		let tbuffer = tf.buffer([1000]) // create a one dimensional tf.buffer of length 1000
+
+		var imagenet = IMAGENET_CLASSES
+
+		// Sets the ID of the top prediction in the buffer to a value of 1
+		Object.keys(imagenet).forEach(function(key) 
+		{
+			if (imagenet[key].valueOf() == targetPrediction.valueOf()) 
+			{
+				tbuffer.set(1, parseInt(key));
+				console.log("Found targeted prediction")
+			}
+		})
+
+  		const targetLabel = tbuffer.toTensor()
+
+	    // x_to_y - A Rank 1 tensor (Vector) of size 1000 - Contains the raw prediction results 
+		// infer: Computes the logits (or the embedding) for the provided image. //prediction?	    
+	    const xtoy = x => { return model.infer(x.toFloat(), 'conv_preds').flatten()} 
+
+		// y_loss - Rank 0 tensor (Scalar) - Contains the top 1 prediction %
+		// SoftmaxCrossEntropy: Normalises numbers into a probability distribition that sums to 1
+	    const yloss = (gt, x) => tf.losses.softmaxCrossEntropy(gt, xtoy(x))
+
+	    // TODO: are these functions  neccesarry due to the way in which the tf.gradients function works?
+	    var loss_func = function(x) { return yloss(targetLabel, x)}
+	    let _grad_func = function () { return loss_func(img3)}
+
+		/**
+		* Returns gradients of `f` with respect to each of the `xs`. The gradients
+		* returned are of the same length as `xs`, but some might be null if `f` was
+		* not a function of that `x`. It also takes optional dy to multiply the
+		* gradient, which defaults to `1`.
+		*/		    
+	    var _im = tf.environment.ENV.engine.gradients(_grad_func, [img3]) 	// var g = tf.grads(_grad_func);
+	    let im_gradients = _im.grads[0]	  									// let test = g([img3])		
+
+		var perturbations = this.scaleGradient(im_gradients, epsilon)
+
+		return perturbations
+	}
+
+
 	/**
 	* TODO..
 	* @param {epsilon} determines the amount of perturbation applied
@@ -87,8 +136,16 @@ export class AdvService
 		// concat the all-zeros alpha channel with 3-channel gradients from tf.gradients()
 		let alphaChannel = tf.tensor3d(zeroes, [227, 227, 1])  // [0, 0,... 0, 0] (227)
 		let perturbationWithAlpha = tf.concat([perturbation, alphaChannel], 2) // 227,227,4   
-		let perturbedImg = tf.add(tf.cast(img4,'float32'), perturbationWithAlpha)
-			
+		//let perturbedImg = tf.add(tf.cast(img4,'float32'), perturbationWithAlpha)
+
+		//console.log('image before')
+		//console.log(tf.cast(img4,'float32').print())
+
+		let perturbedImg = tf.sub(tf.cast(img4,'float32'), perturbationWithAlpha)
+
+		//console.log('Perturbed image after')
+		//console.log(perturbedImg.print())
+
 		return perturbedImg
 	}
 
--- src/app/selection/selection.component.ts
+++ src/app/selection/selection.component.ts
@@ -5,6 +5,7 @@ import { AdvService } from '../adv.service';
 import { TransferService } from '../transfer.service';
 import { ModelData } from '../ModelData';
 import * as tf from '@tensorflow/tfjs';
+import {IMAGENET_CLASSES} from '@tensorflow-models/mobilenet/dist/imagenet_classes';
 
 @Component({
   selector: 'app-selection',
@@ -14,8 +15,11 @@ import * as tf from '@tensorflow/tfjs';
 export class SelectionComponent implements OnInit 
 {
 	imgURL: string = 'assets/images/cat299.jpg'
-	epsilon: number = 0.025
+	epsilon: number = 5
 	selectedModel: string
+	targetClass: string
+
+	imageNet: string[]
 
   	constructor(
 		private modelService: ModelService,
@@ -29,6 +33,18 @@ export class SelectionComponent implements OnInit
 	ngOnInit() 
 	{
 		this.modelService.loadAllModels()
+		this.parseIMAGENET()
+	}
+
+
+	parseIMAGENET()
+	{
+		this.imageNet = new Array()
+
+		for(var i = 0; i < 1000; i++)
+		{
+			this.imageNet.push(IMAGENET_CLASSES[i])
+		}
 	}
 
 	onSelectFile(event) 
@@ -89,9 +105,13 @@ export class SelectionComponent implements OnInit
 		    // 4-channel img with alpha
 		    const img4 = tf.image.resizeBilinear(tf.fromPixels(canvasOriginal, 4), [227, 227])		
 
-			
+
+		    if(this.targetClass == null)
+		    	return console.error('No target class selected for T-FGSM attack')
+
+		    //TargetedFGSM Attempt
 			// Generate the raw adversarial perturbation
-			this.advService.genAdvPerturbation(selectedModel, predictions[0].className, img3, img4, this.epsilon).then(perturbation => 
+			this.advService.genAdvPerturbationFGSMTargeted(selectedModel, this.targetClass, img3, img4, this.epsilon).then(perturbation => 
 			{	
 				// Apply an alpha layer to the raw perturbation, to allow it to be seen
 				// TODO: this will need to be scaled at low epsilon values, or it will not be noticable
@@ -121,6 +141,44 @@ export class SelectionComponent implements OnInit
 					})	
 				})
 			})
+
+
+
+
+
+
+			
+			// // Generate the raw adversarial perturbation
+			// this.advService.genAdvPerturbation(selectedModel, predictions[0].className, img3, img4, this.epsilon).then(perturbation => 
+			// {	
+			// 	// Apply an alpha layer to the raw perturbation, to allow it to be seen
+			// 	// TODO: this will need to be scaled at low epsilon values, or it will not be noticable
+			// 	var drawPerturbation = this.advService.applyAlphaChannel(perturbation)
+
+			// 	// draw the raw perturbation to the canvas: BEFORE it is combined with the original image to generate the full adversarial example
+			// 	this.imageService.drawTensorToCanvas('canvasDifference', drawPerturbation, 299, 299)		
+
+			// 	// get prediction for the raw perturbation
+			// 	this.modelService.tryPredict(selectedModel, canvasDifference).then(modelOutput =>
+			// 	{
+			// 		let predictions = this.modelService.decodeOutput(selectedModel, modelOutput, 5)
+			// 		this.transferService.setDifferencePredictions(predictions)							
+			// 	})	
+
+			// 	// combine the original image and the perturbation
+			// 	this.advService.combineImgAndPerturbation(img4, perturbation).then(perturbedImgTensor => 
+			// 	{
+			// 		this.imageService.drawTensorToCanvas('canvasAdversarial', perturbedImgTensor, 299, 299)		
+
+			// 		// TODO: re-classification SHOULD be done with the raw peturbedIMGTensor, re-sizing for canvas will break things?
+			// 		// Currently it is being done with the re-sized canvas
+			// 		this.modelService.tryPredict(selectedModel, canvasAdversarial).then(modelOutput =>
+			// 		{
+			// 			let predictions = this.modelService.decodeOutput(selectedModel, modelOutput, 5)
+			// 			this.transferService.setAdversarialPredictions(predictions)							
+			// 		})	
+			// 	})
+			// })
 		})
 	}
 
