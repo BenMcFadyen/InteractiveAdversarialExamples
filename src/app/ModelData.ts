f680e12602c6c643af924b05190bb74f8cc279de
BenMcFadyen <ben.mcfadyen3@gmail.com>
2019-03-17 16:47:46 +0000

    added parameter documenation

--- src/app/ModelData.ts
+++ src/app/ModelData.ts
@@ -14,6 +14,20 @@ export class ModelData
 	batchInput:boolean
 	normaliseImage:boolean
 
+	/**
+	* @constructor
+ 	* @param {string} 	name - Name of the model
+ 	* @param {tf.Model} model - tensorflow.js model object, used for prediction
+ 	* @param {boolean}  loaded - True if model is loaded
+ 	* @param {number} 	imgHeight - Required image height (pixels) for prediction
+ 	* @param {number} 	imgWidth - Required image width (pixels) for prediction
+ 	* @param {number} 	imgChannels - Required image channels for prediction
+ 	* @param {Array<any>} classLabels - Class labels that match the tf.model returns
+ 	* @param {boolean} 	applySoftMax - Set true if softmax NEEDS applying to model.predict output
+ 	* @param {string} 	predictionOutputLayer - The name of the layer at which model logits can be taken BEFORE an activation is applied (e.g softMax)
+ 	* @param {boolean} 	batchInput - Set true if model requires input to be batched for prediction
+ 	* @param {boolean} 	normaliseImage - Set true if images are required to be normalised within the range [-1,1] before prediction 	
+	*/
 	constructor(name:string, 	 			imgHeight:number, 
 				imgWidth:number, 			imgChannels:number, 
 				classLabels, 				applySoftMax:boolean, predictionOutputLayer:string = null,
--- src/app/Prediction.ts
+++ src/app/Prediction.ts
@@ -3,6 +3,11 @@ export class Prediction
 	className:string;
 	confidence: number;
 
+	/**
+	* @constructor
+ 	* @param {string} className - The name of the class to which this prediction belongs
+ 	* @param {number} confidence - The confidence (%) of the predicted className
+	*/
 	constructor(className:string, confidence:number)
 	{
 		this.className = className;
