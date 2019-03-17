8e8f9895c8e250a4b0221dcd910a693d1768f9d9
BenMcFadyen <ben.mcfadyen3@gmail.com>
2019-03-17 15:40:57 +0000

    major refactor to support models other than mobilenet -added modularity for classLabels -added validation for various model-specific params (normalisation, pred layers) -added type casting to return types -moved alpha channel application to imageService -added reverseSoftMaxFunction (not currently used) -removed dependence on imagenet classes from mobilenet

--- src/app/adv.service.ts
+++ src/app/adv.service.ts
@@ -4,7 +4,8 @@ import { ModelService } from './model.service';
 import { ModelData } from './ModelData';
 
 import * as tf from '@tensorflow/tfjs';
-import {IMAGENET_CLASSES} from '@tensorflow-models/mobilenet/dist/imagenet_classes';
+import {IMAGENET_CLASSES} from './ImageNetClasses';
+import * as mobilenet from '@tensorflow-models/mobilenet';
 
 enum CombineMethod 
 {
@@ -12,84 +13,98 @@ enum CombineMethod
   Subtract,
 }
 
-
 @Injectable({
   providedIn: 'root'
 })
 
-
 export class AdvService 
 {
-	constructor(private imageService:ImageService) 
-	{
- 
-
- 	}
+	constructor(private imgService:ImageService, private modelService:ModelService) {}
 
 	/**
 	* Generates perturbation in respect to the provided targetClass
 	* @return {tf.Tensor} perturbed img tensor
 	*/
-	private async singleStepAttack(model: tf.Model, targetClass: string, img3, img4, epsilon = 1)
+	private async singleStepAttack(modelObj: ModelData, classLabels, targetClass, img3, epsilon)
 	{
-		let tbuffer = tf.buffer([1000]) // create a one dimensional tf.buffer of length 1000 
-		var labelClasses = IMAGENET_CLASSES //TODO: Add modularity here
-
-		// Sets the ID of the targetClass in buffer to a value of 1
-		Object.keys(labelClasses).forEach(function(key) 
+		return tf.tidy(()=>
 		{
-			if (labelClasses[key].valueOf() == targetClass.valueOf()) 
-				tbuffer.set(1, parseInt(key));
+			let tbuffer = tf.buffer([1000]) // used for storing the one-hot label
+
+			// one-hot encode the target class  //TODO: Use tf.oneHot?
+			Object.keys(classLabels).forEach(function(key) 
+			{
+				if (classLabels[key].valueOf() == targetClass.valueOf()) 
+					tbuffer.set(1, parseInt(key));
+			})
+		
+	  		const oneHotClassLabels = tbuffer.toTensor()
+
+	  		if(modelObj.normaliseImage)
+	  			img3 = this.imgService.normaliseIMGTensor(img3)
+
+	  		let model = modelObj.model
+
+	  		// if a predictionOutpulLayer is set, use this as the model to ensure we get the needed logits (before a softmax layer for example)
+	  		if(modelObj.predictionOutputLayer != null)
+	  			 model = this.getModelAtEndpoint(modelObj.model, modelObj.predictionOutputLayer)
+
+	  		const getModelLogits = x => model.predict(x.toFloat()).as1D() //must be the RAW prediction logits, BEFORE any activation functions, or the gradient calculated will not be correct
+		    const lossFunction = x => tf.losses.softmaxCrossEntropy(oneHotClassLabels, getModelLogits(x))
+		    const gradientFunction = tf.grad(lossFunction)
+		    var gradient = gradientFunction(img3)
+			var perturbation = this.scaleGradient(gradient, epsilon)
+
+			return perturbation
 		})
-
-  		const oneHotLabels = tbuffer.toTensor()
-
-
-  		const getModelLogits = x => model.infer(x.toFloat(), 'conv_preds').as1D()
-
-	    const lossFunction = x => tf.losses.softmaxCrossEntropy(oneHotLabels, getModelLogits(x))
-
-	    const gradientFunction = tf.grad(lossFunction)
-
-	    var gradient = gradientFunction(img3)
-
-
-	    console.log(img3)
-	   	  									
-		var perturbation = this.scaleGradient(gradient, epsilon)
-
-		return perturbation
 	}
 
 
-
-	async FGSM(model: tf.Model, orginalPrediction: string, img3, img4, epsilon = 0.025)
+	reverseSoftmax(softmaxNums, constant)
 	{
-		return this.singleStepAttack(model, orginalPrediction, img3, img4, epsilon).then(perturbation =>
+		softmaxNums = softmaxNums.dataSync()
+		let revSoftMaxNums = new Array()
+
+		for(let i = 0; i < softmaxNums.length; i++)	
 		{
-			// For FGSM ADD the perturbation to the img (move AWAY the gradient)			
+			let x = Math.log(softmaxNums[i]) + constant
+			revSoftMaxNums.push(x)
+		}
+
+		return revSoftMaxNums
+	}
+
+
+	getModelAtEndpoint(model: tf.Model, endpoint:string)
+	{
+		return tf.model({inputs:model.inputs, outputs:model.getLayer(endpoint).output})
+	}
+
+
+	async FGSM(modelObj: ModelData, classLabels, orginalPrediction: string, img3, img4, epsilon = 1)
+	{		
+		return this.singleStepAttack(modelObj, classLabels, orginalPrediction, img3, epsilon).then(perturbation =>
+		{
+			// For FGSM ADD the perturbation to the img (move AWAY from the gradient)			
 			return this.combineImgAndPerturbation(img4, perturbation, CombineMethod.Add).then(perturbedImgTensor => 
 			{
-				return perturbedImgTensor
+				return <tf.Tensor3D | tf.Tensor4D> perturbedImgTensor
 			})
 		})
 	}
 
 
-
-	async Targeted_FGSM(model: tf.Model, targetPrediction:string, img3, img4, epsilon = 0.025)
+	async Targeted_FGSM(modelObj: ModelData, classLabels, targetPrediction:string, img3, img4, epsilon = 1)
 	{
-		return this.singleStepAttack(model, targetPrediction, img3, img4, epsilon).then(perturbation =>
+		return this.singleStepAttack(modelObj, classLabels, targetPrediction, img3, epsilon).then(perturbation =>
 		{
 			// For Targeted-FGSM SUBTRACT the perturbation from the img (move TOWARDS the gradient)
 			return this.combineImgAndPerturbation(img4, perturbation, CombineMethod.Subtract).then(perturbedImgTensor => 
 			{
-				return perturbedImgTensor
+				return <tf.Tensor3D | tf.Tensor4D> perturbedImgTensor
 			})
 		})
 	}
-
-
 
 	/**
 	* TODO..
@@ -99,28 +114,13 @@ export class AdvService
 	private async combineImgAndPerturbation(img4, perturbation, combineMethod:CombineMethod)
 	{
 		// apply alpha layer to the perturbation
-		let perturbationWithAlpha = this.applyAlphaChannel(perturbation)
+		let perturbationWithAlpha = this.imgService.applyAlphaChannelToTensor(perturbation, 0)
 
 		if(combineMethod == CombineMethod.Add)
 			return tf.add(tf.cast(img4,'float32'), perturbationWithAlpha)
 		
 		return tf.sub(tf.cast(img4,'float32'), perturbationWithAlpha)
 
-	}
-
-	/**
-	* TODO: Assumes Rank3 img tensor, Add modularity here
-	* @param {epsilon} determines the amount of perturbation applied
-	* @return {tf.Tensor} perturbed img tensor
-	*/
-	private applyAlphaChannel(tensorIMG, numberToFillWith:number = 0)
-	{
-		var arraySize = tensorIMG.shape[0] * tensorIMG.shape[1] //TODO: Assumes Rank3 img tensor
-		const filler = new Uint8Array(arraySize).fill(numberToFillWith)
-		// concat the all-zeros alpha channel with 3-channel gradients from tf.gradients()		
-		let alphaChannel = tf.tensor3d(filler, [tensorIMG.shape[0], tensorIMG.shape[1], 1])// [0, 0,... 0, 0] (227)
-		let tensorWithAlpha = tf.concat([tensorIMG, alphaChannel], 2) // 227,227,4   
-		return tensorWithAlpha
 	}
 
 
