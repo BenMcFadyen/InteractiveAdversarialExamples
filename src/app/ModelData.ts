5bc301e8c941d8a66ffbd50d45e9fc85f97d22a3
BenMcFadyen <ben.mcfadyen3@gmail.com>
2019-03-17 15:37:05 +0000

    added additional model parameters applySoftmax -if softmax is not being applied by the model predictionOutputLayer - layer at which model will be sliced to get the raw prediction logits, before any activation layers batchInput - if model requires input to be batched

--- src/app/ModelData.ts
+++ src/app/ModelData.ts
@@ -2,24 +2,31 @@ import * as tf from '@tensorflow/tfjs';
 
 export class ModelData
 {
-	name: string;
-	model: tf.Model;
-	loaded: boolean;
-	imgHeight:number;
-	imgWidth:number;
-	imgChannels:number;
-	classLabels:Array<any>;
+	name: string
+	model: tf.Model
+	loaded: boolean
+	imgHeight:number
+	imgWidth:number
+	imgChannels:number
+	classLabels:Array<any>
+	applySoftMax:boolean
+	predictionOutputLayer:string
+	batchInput:boolean
 	normaliseImage:boolean
 
-	constructor(name:string, imgHeight:number, 
-				imgWidth:number, imgChannels:number, 
-				classLabels, normaliseImage:boolean = true)
+	constructor(name:string, 	 			imgHeight:number, 
+				imgWidth:number, 			imgChannels:number, 
+				classLabels, 				applySoftMax:boolean, predictionOutputLayer:string = null,
+				batchInput:boolean = true, 	normaliseImage:boolean = true)
 	{
-		this.name = name;
-		this.imgHeight = imgHeight;
-		this.imgWidth = imgWidth;	
-		this.imgChannels = imgChannels;
-		this.classLabels = classLabels;
-		this.normaliseImage = normaliseImage;
+		this.name = name
+		this.imgHeight = imgHeight
+		this.imgWidth = imgWidth
+		this.imgChannels = imgChannels
+		this.classLabels = classLabels
+		this.applySoftMax = applySoftMax
+		this.predictionOutputLayer = predictionOutputLayer
+		this.batchInput = batchInput
+		this.normaliseImage = normaliseImage
 	}
 }
