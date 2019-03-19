27b4a96990ad95e83f066e6377720d9ec05b8616
BenMcFadyen <ben.mcfadyen3@gmail.com>
2019-03-19 20:12:27 +0000

    added duplicate model check to prevent overloading of the display

--- src/app/transfer.service.ts
+++ src/app/transfer.service.ts
@@ -24,6 +24,8 @@ export class TransferService
 	private adversarialImageModelNameSource = new BehaviorSubject(this.adversarialImageModelName);
 	currentAdversarialImageModelNameSource = this.adversarialImageModelNameSource.asObservable();
 
+
+
 	constructor() 
 	{ 
 
@@ -40,26 +42,53 @@ export class TransferService
 	}
 
 
+	checkDuplicateModel(modelName:string):boolean
+	{
+		if(this.allModelPredictions == null)
+			return 
+
+		for(var i = 0; i < this.allModelPredictions.length; i++)
+		{
+			if(this.allModelPredictions[i].modelName == modelName)
+			{
+				this.allModelPredictions.splice(i,1)// remove the element from the array
+				return true //TODO potentially insert here too, to prevent the models jumping around when switching
+			}
+		}
+
+		return false
+	}
+
+
 	addNewModelPrediction(newModelPrediction: ModelPrediction, clearPrevious:boolean)
 	{
 
 		if(clearPrevious)
 		{	
-			// if newModelPrediction is null, clear the prediction array, this will hide the prediction table
+			// if newModelPrediction is null, set predictions to null, this will hide the prediction div element
 			if(newModelPrediction == null)
 			{
-				this.allModelPredictions == null
+				this.allModelPredictions = null
 				this.allModelPredictionsSource.next(null)
+				console.log('All model preds: 1')				
+				console.log(this.allModelPredictions)
 				return
 			}
 
 			this.allModelPredictionsSource.next([newModelPrediction])
+			console.log('All model preds: 1')							
+			console.log(this.allModelPredictions)			
 			return
 		}
 
 		if(this.allModelPredictions == null)
 			this.allModelPredictions = new Array()
 
+
+		// checks if newModelPrediction is already a prediction, if it is, removes this.
+		this.checkDuplicateModel(newModelPrediction.modelName)
+
+		// push the new prediction to the array, then set the source
 		this.allModelPredictions.push(newModelPrediction)
 		this.allModelPredictionsSource.next(this.allModelPredictions)
 	}
