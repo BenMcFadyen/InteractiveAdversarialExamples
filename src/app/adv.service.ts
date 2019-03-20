0c9cd1b64ced035b119e40baac64ff8fb5bdcc52
BenMcFadyen <ben.mcfadyen3@gmail.com>
2019-03-20 11:59:19 +0000

    perturbation can now be drawn to the canvas -rename alphaChannel method -rename getModelatEndpoint method -add comments to some functions -added method for creation of filled tensor

--- src/app/adv.service.ts
+++ src/app/adv.service.ts
@@ -45,36 +45,22 @@ export class AdvService
 
 	  		let model = modelObj.model
 
-	  		// if a predictionOutpulLayer is set, use this as the model to ensure we get the needed logits (before a softmax layer for example)
+	  		// if a predictionOutpulLayer is set, use this as the model to ensure we get the needed logits before any activation function
 	  		if(modelObj.predictionOutputLayer != null)
-	  			 model = this.getModelAtEndpoint(modelObj.model, modelObj.predictionOutputLayer)
+	  			 model = this.getModelAtSpecificEndpoint(modelObj.model, modelObj.predictionOutputLayer)
 
 	  		const getModelLogits = x => (<tf.Tensor> model.predict(x.toFloat())).as1D() //must be the RAW prediction logits, BEFORE any activation functions, or the gradient calculated will not be correct
 		    const lossFunction = x => tf.losses.softmaxCrossEntropy(oneHotClassLabels, getModelLogits(x))
 		    const gradientFunction = tf.grad(lossFunction)
 		    var gradient = gradientFunction(img3)
-			var perturbation = this.scaleGradient(gradient, epsilon)
+			var perturbation = this.scaleTensor(gradient, epsilon)
 
 			return perturbation
 		})
 	}
 
-
-	reverseSoftmax(softmaxNums, constant)
-	{
-		softmaxNums = softmaxNums.dataSync()
-		let revSoftMaxNums = new Array()
-
-		for(let i = 0; i < softmaxNums.length; i++)	
-		{
-			let x = Math.log(softmaxNums[i]) + constant
-			revSoftMaxNums.push(x)
-		}
-
-		return revSoftMaxNums
-	}
-
-	getModelAtEndpoint(model: tf.Model, endpoint:string)
+	/** Used to create a model sliced at one of its layers (used to retreive the model logits before any activation function is applied*/
+	getModelAtSpecificEndpoint(model: tf.Model, endpoint:string)
 	{
 		return tf.model({inputs:model.inputs, outputs:model.getLayer(endpoint).output})
 	}
@@ -95,13 +81,38 @@ export class AdvService
 	async Targeted_FGSM(modelObj: ModelData, targetPrediction:string, img3, img4, epsilon = 1)
 	{
 		return this.singleStepAttack(modelObj, targetPrediction, img3, epsilon).then(perturbation =>
-		{
+		{	
+
+			let differenceScaleValue = 50; //TODO: Let the user control this value?
+			this.drawPerturbationToCanvas(perturbation, differenceScaleValue)
+
 			// For Targeted-FGSM SUBTRACT the perturbation from the img (move TOWARDS the gradient)
 			return this.combineImgAndPerturbation(img4, perturbation, CombineMethod.Subtract).then(perturbedImgTensor => 
 			{
 				return <tf.Tensor3D | tf.Tensor4D> perturbedImgTensor
 			})
 		})
+	}
+
+
+	/**
+	* As canvas' are transparant by default the perturbation cannot be drawn to the canvas by default
+	* Here the perturbation is combined with an all-white tensor, so the perturbation is visible when drawn
+	*/
+	drawPerturbationToCanvas(perturbation:tf.Tensor3D | tf.Tensor4D, differenceScaleValue:number = 50)
+	{
+		// scale the perturbation as required (low epsilon must be scaled to be visible)
+		let scaledPerturbation = this.scaleTensor(perturbation, differenceScaleValue) 
+
+		// apply alpha channel to perturbation (ensure it is the correct shape for canvas display)
+		let perturbationWithAlpha = this.imgService.createAndApplyAlphaChannelToTensor(scaledPerturbation, 255)
+
+		// Create the all-white tensor (pixel-value: 255)
+		let whiteTensor = this.imgService.createFilledTensor(perturbationWithAlpha.shape, 255)
+
+		// combine the white-tensor with the perturbed one; draw to canvas
+		let whitePerturbedTensor = <tf.Tensor3D | tf.Tensor4D> whiteTensor.add(perturbationWithAlpha)
+		this.imgService.drawTensorToCanvas('canvasDifference', whitePerturbedTensor, 350, 350)		
 	}
 
 	/**
@@ -112,29 +123,45 @@ export class AdvService
 	private async combineImgAndPerturbation(img4, perturbation, combineMethod:CombineMethod)
 	{
 		// apply alpha layer to the perturbation
-		let perturbationWithAlpha = this.imgService.applyAlphaChannelToTensor(perturbation, 0)
+		let perturbationWithAlpha = this.imgService.createAndApplyAlphaChannelToTensor(perturbation, 0)
 
 		if(combineMethod == CombineMethod.Add)
-			return tf.add(tf.cast(img4,'float32'), perturbationWithAlpha)
+			return <tf.Tensor3D | tf.Tensor4D> tf.add(tf.cast(img4,'float32'), perturbationWithAlpha)
 		
-		return tf.sub(tf.cast(img4,'float32'), perturbationWithAlpha)
-
+		return <tf.Tensor3D | tf.Tensor4D> tf.sub(tf.cast(img4,'float32'), perturbationWithAlpha)
 	}
 
 	/**
-	* TODO..
-	* @param {epsilon} determines the amount of perturbation applied
+	* Scale the given tensor according to the given epsilon value
+	* Example: tensor = [-0.01, 1, -0.1, 3], epsilon = 10
+	* 		   scaled = [-10, 10, -10, 10]
+	* @param {epsilon} determines the scale of the perturbation
 	* @return {tf.Tensor} perturbed img tensor
 	*/
-	private scaleGradient(gradient, epsilon) 
+	private scaleTensor(tensor, epsilon) 
 	{
-		const gradientData = gradient.dataSync()
+		const tensorData = tensor.dataSync()
 
-		const normalizedGradient = gradientData.map(x => 
+		const scaledTensor = tensorData.map(x => 
 		{
 			return epsilon * Math.sign(x)
 		})
 
-		return tf.tensor(normalizedGradient).reshapeAs(gradient)
+		return tf.tensor(scaledTensor).reshapeAs(tensor)
+	}	
+
+	/** Experimental (not used) */
+	reverseSoftmax(softmaxNums, constant)
+	{
+		softmaxNums = softmaxNums.dataSync()
+		let revSoftMaxNums = new Array()
+
+		for(let i = 0; i < softmaxNums.length; i++)	
+		{
+			let x = Math.log(softmaxNums[i]) + constant
+			revSoftMaxNums.push(x)
+		}
+
+		return revSoftMaxNums
 	}	
 }
