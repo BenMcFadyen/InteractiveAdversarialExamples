eec750a66acc8183de0e5947e6a23f4e9a9e92c6
BenMcFadyen <ben.mcfadyen3@gmail.com>
2019-03-23 19:55:08 +0000

    refactored the display component to use material tables

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -13,18 +13,19 @@ import { TransferService } from '../transfer.service';
 export class DisplayComponent implements OnInit
 {
  	allModelPredictions:ModelPrediction[]
+
  	adversarialImageModelNameSource:string 
+
+	columnsToDisplay = ['className', 'confidence'];
+
 
 	constructor(private tferService: TransferService) 
 	{
-
-	
 	}
 
 	ngOnInit() 
 	{
 		this.tferService.currentAllModelPredictionsSource.subscribe(allModelPredictions => this.allModelPredictions = allModelPredictions)
 		this.tferService.currentAdversarialImageModelNameSource.subscribe(adversarialImageModelNameSource => this.adversarialImageModelNameSource = adversarialImageModelNameSource)
-
 	}
 }
