9dea3c422a6d54bddd912b3ffe56323f714315f1
BenMcFadyen <ben.mcfadyen3@gmail.com>
2019-03-24 18:43:21 +0000

    added nuimber of layers stat

--- src/app/ModelStats.ts
+++ src/app/ModelStats.ts
@@ -2,15 +2,17 @@ export class ModelStats
 {
 	name: string
 	size: number
+	layers:number
 	top1: number
 	top5:number
 	parameters:number
 	requestLoad:boolean
 
-	constructor(name:string, size:number, top1:number, top5:number, parameters:number, requestLoad:boolean)
+	constructor(name:string, size:number, layers:number, top1:number, top5:number, parameters:number, requestLoad:boolean)
 	{
 		this.name = name
 		this.size = size
+		this.layers = layers
 		this.top1 = top1
 		this.top5 = top5
 		this.parameters = parameters
