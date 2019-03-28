f91f3e9185d1085e5212e7bb4a2222e6538824d8
BenMcFadyen <ben.mcfadyen3@gmail.com>
2019-03-28 19:29:05 +0000

    limit to mobilenet models only -removed disclaimer

--- src/app/dialogs/model-select-dialog/model-select-dialog.component.ts
+++ src/app/dialogs/model-select-dialog/model-select-dialog.component.ts
@@ -154,15 +154,15 @@ export class ModelSelectDialogComponent implements OnInit
 		this.totalSize = Math.round(this.totalSize * 100) / 100
 
 
-		if(!this.disclaimerShown && this.modelsToLoad.length >= 3)
-		{
-			let modelSelectDisclaimerDialogRef = this.openModelSelectDisclaimerDialog()
+		// if(!this.disclaimerShown && this.modelsToLoad.length >= 3)
+		// {
+		// 	let modelSelectDisclaimerDialogRef = this.openModelSelectDisclaimerDialog()
 
-			modelSelectDisclaimerDialogRef.afterClosed().subscribe(() => 
-			{
-				this.disclaimerShown = true;			
-			})			
-		}
+		// 	modelSelectDisclaimerDialogRef.afterClosed().subscribe(() => 
+		// 	{
+		// 		this.disclaimerShown = true;			
+		// 	})			
+		// }
 	}
 
 
